961bbaff9fdd7736a7925923253f821c22dda324
Saikishor Rasala <rasalasaikishor@gmail.com>
2023-10-20 22:50:17 +0530

    added navbar content

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/no-unescaped-entities */
+
+import { BrowserRouter } from 'react-router-dom';
+
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from './components';
 
 
 const App = () => {
 
   return (
-    <div>
-      Saikishor's Portfolio
-    </div>
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className="relative z-0">
+          <contact />
+          <StarsCanvas />
+        </div>
+      </div>
+    </BrowserRouter>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+
+import { styles } from '../styles';
+import { navLinks } from '../constants';
+import { logo, menu, close } from '../assets';
+
 
 const Navbar = () => {
+  const [active, setactive] = useState('')
   return (
-    <div>Navbar</div>
+    <nav
+    className={`${styles.paddindX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
+    >
+
+      <div className="w-full flex flex justify-between items-center max-w-7xl mx-auto">
+        <link
+          to="/"
+          className="flex items-center gap-2"
+          onClick={() => {
+          setActive("");
+          window.scrollTo(0, 0);
+          }}
+        >
+          {/* <img src={logo} alt="logo" className="w-9 h-9 object-contain" /> */}
+        </link>
+      </div>
+
+    </nav>
   )
 }
 
