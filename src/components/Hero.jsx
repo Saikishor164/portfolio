6de4e0e59f4b128f158c5003a259aa487ce183ae
Saikishor Rasala <rasalasaikishor@gmail.com>
2023-10-22 13:59:33 +0530

    added name on the hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import { motion } from 'framer-motion';
+
+import { styles } from '../styles'
+import { ComputersCanvas } from './canvas';
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section className="relative w-full h-screen mx-auto" > 
+      <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
+        <div className="flex flex-col justify-center items-center mt-5">
+          <div className="w-5 h-5 rounded-full bg-[#915eff]" />
+          <div className="w-1 sm:h-80 h-40 violet-gradient" />
+
+        </div>
+
+        <div>
+          <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className="text-[#ADD8E6]">Saikishor</span></h1>
+          <p className="{`${styles.heroSubText} mt-2 text-white-100`}" > I Devlop Websites that work for you.
+          </p>
+        </div>
+      </div>
+      <ComputersCanvas />
+
+     </section>
   )
 }
 
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,6 +1,15 @@
-import React from 'react'
+import {Suspense, useEffect, useState} from 'react';
+
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
+
+import CanvasLoader from '../Loader';
+
 
 const Computers = () => {
+  const computer = useGLTF('./desktop_pc/scene.gltf')
+
+
   return (
     <div>Computers</div>
   )
