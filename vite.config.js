bf88b762b1994b02c506814704295fcb0c0c8c47
saikishor164 <saikishorrasala418@gmail.com>
2024-07-22 10:59:43 +0530

    Remove unused components and configuration files

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import VueImages from 'vite-plugin-vue-images';
-
-export default defineConfig({
-  plugins: [react(), VueImages()],
-});
