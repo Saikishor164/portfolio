80a89be0e85b9d65ea507e9a5cd6ea3223ccecc1
saikishor164 <saikishorrasala418@gmail.com>
2024-07-22 11:18:27 +0530

    chore: optimize App.js rendering for better performance and added images has assets in src and public folder.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
+  return (  
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
